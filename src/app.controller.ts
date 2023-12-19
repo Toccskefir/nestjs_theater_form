9e9869c97ae0bf1de1eb429b4ffc374c99e83901
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-19 12:42:37 +0100

    Adding data to the database now works

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import * as mysql from 'mysql2';
 import { AppService } from './app.service';
 import { NewCuponDto } from './newCuponDto';
@@ -26,6 +26,37 @@ export class AppController {
   @Get('/form')
   @Render('form')
   form() {
-    return { title: 'Kupon hozzáadása' };
+    return { title: 'Kupon hozzáadása', errors: [] };
+  }
+
+  @Post('/form')
+  @Render('form')
+  async formPost(@Body() newCupon: NewCuponDto, @Res() res: Response) {
+    const errors: string[] = [];
+    const codeRegex = /^[A-Z]{4}-[0-9]{6}$/;
+
+    if (newCupon.title.trim() === '') {
+      errors.push('Adja meg az előadás címét!');
+    }
+
+    if (newCupon.percentage < 1 || isNaN(newCupon.percentage)) {
+      errors.push('A kedvezmény értéke legalább 1% kell legyen!');
+    }
+
+    if (newCupon.code.trim() === '') {
+      errors.push('Adja meg a kupon kódját!');
+    } else if (codeRegex.test(newCupon.code) === false) {
+      errors.push('A kupon kódja nem megfelelő formátumú! (pl.: ABCD-123456)');
+    }
+
+    if (errors.length > 0) {
+      res.render('form', { title: 'Kupon hozzáadása', errors });
+    } else {
+      const title: string = newCupon.title;
+      const percentage: number = newCupon.percentage;
+      const code: string = newCupon.code;
+      const [data] = await conn.execute('INSERT INTO cupons (title, percentage, code) VALUES (?, ?, ?)', [title, percentage, code]);
+      res.redirect('/');
+    }
   }
 }
